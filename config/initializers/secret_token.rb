6418cfe41294021f3309fda3904230875d8ea41e
ztratar <ztratar@gmail.com>
2012-04-28 18:50:55 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Airety::Application.config.secret_token = 'b3e49026c2055c82318c202b0600a8bed39f8a3a9bee5c8881dcb40d89145939aee5219b31666ef3b9b0dcc03d35f9c8cc4a632f2d9c2cff0dba3335ea55ee9b'
